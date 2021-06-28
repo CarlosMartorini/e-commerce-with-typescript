bf33764ff52390c1c2233ef4bd3c9ad6a4267c73
Carlos Martorini <c7.silveira@gmail.com>
2021-06-27 21:38:04 -0300

    feat: page home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,29 +1,28 @@
 import React, { useEffect, useState } from "react";
 import api from "../../services/api";
-import formatValue from "../../utils/FormatValue";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { Container, ProductList } from "./Styles";
 import { useCart } from "../../providers/cart/Cart";
+import { Product } from "../../types/Product";
 
-function Home() {
 
-
+const Home = () => {
   const { setCart, cart } = useCart();
 
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState<Product[]>([] as Product[]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   async function loadProducts() {
     const response = await api.get("/products/");
 
-    const data = response.data.map((product) => ({
-      ...product,
-      priceFormatted: formatValue(product.price),
-    }));
+    const data = response.data.map((product: Product) => ({
+    ...product,
+    price: product.price,
+  }));
 
-    setLoading(false);
-    setProducts(data);
-  }
+  setLoading(false);
+  setProducts(data);
+}
 
   useEffect(() => {
     loadProducts();
@@ -31,30 +30,31 @@ function Home() {
 
   return (
     <Container>
-      {loading ? (
-        <CircularProgress size={50} />
-      ) : (
-        <ProductList>
-          {products.map((product) => (
+    {loading ? (
+      <CircularProgress size={50} />
+    ) : (
+      <ProductList>
+        {
+          products.map((product: Product) => (
             <li key={product.id}>
               <figure>
-                <img src={product.image} alt={product.name} />
+                <img src={product.img} alt={product.title} />
               </figure>
               <strong>{product.title}</strong>
               <div>
-                <span>{product.priceFormatted}</span>
-
-                <button
-                  type="button"
-                  onClick={() => setCart([...cart, product])}
-                >
+                <span>{product.price}</span>
+                  <button
+                    type="button"
+                    onClick={() => setCart([...cart, product])}
+                  >
                   <span>Adicionar ao carrinho</span>
-                </button>
+                  </button>
               </div>
             </li>
-          ))}
+          ))
+        }
         </ProductList>
-      )}
+    )}
     </Container>
   );
 }
