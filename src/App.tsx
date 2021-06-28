4e4d11a8527f511119d6413d61bf82a16947ce70
Carlos Martorini <c7.silveira@gmail.com>
2021-06-27 22:28:48 -0300

    feat: add react toastify on App and fix path import routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import GlobalStyles from './global/styles';
 import React from "react";
-import Route from "./routes/Route";
+import Route from "./routes";
 import Header from "./components/header/Header";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Header/>
       <Route/>
       <GlobalStyles/>
